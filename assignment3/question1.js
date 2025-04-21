b9a0452a0a1f0a1e6e1399e9bc309d22945f9541
Ryeo <32667987+y2h1987@users.noreply.github.com>
2025-04-21 22:35:48 +0800

    Assignment 3

---
+++ assignment3/question1.js
@@ -0,0 +1,7 @@
+const msftData = [190.15, 194.21, 191.07, 190.44, 200.15];
+
+// Write your code below
+
+
+let average = (msftData[0] + msftData[1] + msftData[2] + msftData[3] + msftData[4]) / 5
+console.log (average)
